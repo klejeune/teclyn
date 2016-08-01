bf6a627b7a632a883207dc758628b842755bf27b
Kevin Le Jeune <kevin-git@le-jeune.fr>
2016-08-01 13:22:32 +0200

    Teclyn MVC controllers

---
+++ src/pcl/Teclyn/Teclyn.AspNetMvc/VirtualFileSystem/TeclynVirtualFile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace Teclyn.AspNetMvc.VirtualFileSystem
+{
+    public class TeclynVirtualFile : VirtualFile
+    {
+        private string resourceName;
+
+        private static string ConvertVirtualPath(string origin)
+        {
+            return typeof(TeclynVirtualFile).Assembly.GetName().Name
+                 + ".Mvc."
+                 + VirtualPathUtility.MakeRelative("~/Areas/Teclyn/Views", origin)
+                     .Replace('/', '.');
+        }
+
+        public TeclynVirtualFile(string virtualPath) : base(virtualPath)
+        {
+            this.resourceName = ConvertVirtualPath(virtualPath);
+        }
+
+        public override Stream Open()
+        {
+            var manifestResourceName = this.GetType().Assembly.GetManifestResourceNames()
+             .First(name => string.Equals(name, this.resourceName, StringComparison.OrdinalIgnoreCase));
+
+            return this.GetType().Assembly.GetManifestResourceStream(manifestResourceName);
+        }
+        public bool Exists
+        {
+            get
+            {
+                return this.GetType().Assembly.GetManifestResourceNames()
+                    .Any(manifestResourceName => string.Equals(manifestResourceName, this.resourceName, StringComparison.OrdinalIgnoreCase));
+            }
+        }
+    }
+}
