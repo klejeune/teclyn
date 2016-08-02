ddd31765cf0f11a195c2c6f40d882e454e06ef0e
Kevin Le Jeune <kevin-git@le-jeune.fr>
2016-08-02 11:55:09 +0200

    InMemoryRepository crash fix

--- src/pcl/Teclyn/Teclyn.SampleMvc/Controllers/TodoListController.cs
+++ src/pcl/Teclyn/Teclyn.SampleMvc/Controllers/TodoListController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using StructureMap.Attributes;
+using Teclyn.AspNetMvc.Models;
 using Teclyn.Core.Storage;
 using Teclyn.SampleCore.TodoLists.Models;
 
@@ -15,11 +16,26 @@ namespace Teclyn.SampleMvc.Controllers
         public IRepository<ITodoList> TodoLists { get; set; }
 
         // GET: TodoList
-        public ActionResult Index(string id)
+        public ActionResult Index(string id, int? s)
         {
-            var todoList = this.TodoLists.GetById(id);
+            var todoList = this.TodoLists.GetByIdOrNull(id);
 
-            return View(todoList);
+            var a = this.TodoLists.OrderByDescending(c => c.CreationDate);
+
+            if (todoList == null)
+            {
+                var todoLists = new ListModel<ITodoList>(
+                    this.TodoLists.OrderByDescending(c => c.CreationDate),
+                    index => this.Url.Action("Index", new { s = index }),
+                    s.GetValueOrDefault(),
+                    20);
+
+                return View(todoLists);
+            }
+            else
+            {
+                return View("Get", todoList);
+            }
         }
     }
 }
