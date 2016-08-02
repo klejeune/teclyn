73393377b657c9bc27567849e99ef0ba8f852ea1
Kevin Le Jeune <kevin-git@le-jeune.fr>
2016-08-02 11:08:05 +0200

    List Models

---
+++ src/pcl/Teclyn/Teclyn.AspNetMvc/Models/IListModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Teclyn.AspNetMvc.Models
+{
+    public interface IListModel
+    {
+        string PreviousLink { get; }
+        string NextLink { get; }
+        int PreviousMin { get; }
+        int PreviousMax { get; }
+        int NextMin { get; }
+        int NextMax { get; }
+        int Total { get; }
+    }
+}
