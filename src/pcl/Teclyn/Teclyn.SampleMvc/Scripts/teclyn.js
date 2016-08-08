c10bb480b56039019250b53cf021240c1c277036
Kevin Le Jeune <kevin-git@le-jeune.fr>
2016-08-08 18:07:02 +0200

    Teclyn.js

---
+++ src/pcl/Teclyn/Teclyn.SampleMvc/Scripts/teclyn.js
@@ -0,0 +1,87 @@
+﻿
+var teclyn = {
+    $,
+    init: function (jquery) {
+        if (jquery) {
+            this.$ = jquery;
+        } else {
+            this.$ = $;
+        }
+
+        this.initForms();
+    },
+    initForms: function () {
+        var t = this;
+
+        $('.command-form')
+            .submit(function() {
+                var form = this;
+                var previousRequestData = form.teclynRequestData;
+                var newRequestData = t.serializeForm(form);
+
+                if (previousRequestData !== newRequestData) {
+                    form.teclynRequestData = newRequestData;
+                    t.executeRemoteCommand(newRequestData, form, false);
+                }
+
+                return false;
+            });
+    },
+    serializeForm: function (form) {
+        return $(form).serialize();
+    },
+    executeRemoteCommand: function (data, anchor, reload, callback, onSuccess, onFailure) {
+        $(':focus').blur();
+
+        $.ajax({
+            type: 'POST',
+            url: '/teclyn/command/execute',
+            data: data,
+            contentType: false,
+            processData: false,
+            success: function(response) {
+                if (response.Success) {
+                    if (onSuccess) {
+                        onSuccess();
+                    }
+
+                    if (callback) {
+                        callback(response, anchor);
+                    }
+
+                    if (reload) {
+                        location.reload();
+                    }
+                } else {
+                    if (onFailure) {
+                        onFailure();
+                    }
+                }
+            }
+        });
+    }
+};
+
+function dump(data) {
+    alert(dumpInner(data, 0));
+}
+
+function dumpInner(data, level) {
+    var display = '';
+
+    $.each(data, function (index, value) {
+        if (level < 1 && value != null && typeof value === 'object') {
+            value = dumpInner(value, level + 1);
+        }
+
+        var indent = '';
+
+        if (level > 0) {
+            indent = Array(level + 1).join(" ");
+        }
+
+        display += indent + "[" + index + "] = " + value + '\n';
+    });
+
+    return display;
+}
