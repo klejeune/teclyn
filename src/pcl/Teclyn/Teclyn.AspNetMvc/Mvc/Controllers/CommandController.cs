238da92b554fdac897c4be920770cd2b7fedabd8
Kevin Le Jeune <kevin-git@le-jeune.fr>
2016-08-10 13:54:50 +0200

    Teclyn home controller emoved

--- src/pcl/Teclyn/Teclyn.AspNetMvc/Mvc/Controllers/CommandController.cs
+++ src/pcl/Teclyn/Teclyn.AspNetMvc/Mvc/Controllers/CommandController.cs
@@ -1,8 +1,11 @@
-﻿using System.Web.Mvc;
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Web.Mvc;
 using Teclyn.AspNetMvc.Mvc.Models;
 using Teclyn.Core;
 using Teclyn.Core.Commands;
 using Teclyn.Core.Ioc;
+using Teclyn.Core.Storage;
 
 namespace Teclyn.AspNetMvc.Mvc.Controllers
 {
@@ -10,6 +13,9 @@ namespace Teclyn.AspNetMvc.Mvc.Controllers
     {
         [Inject]
         public CommandService CommandService { get; set; }
+
+        [Inject]
+        public RepositoryService RepositoryService { get; set; }
 
         public ActionResult Index()
         {
@@ -36,6 +42,26 @@ namespace Teclyn.AspNetMvc.Mvc.Controllers
             }
 
             return Redirect(returnUrl);
+        }
+
+
+        public ActionResult Info()
+        {
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            var fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            string version = fvi.FileVersion;
+
+            var model = new HomeInfoModel
+            {
+                Aggregates = this.RepositoryService.Aggregates.Select(agg => new AggregateInfoModel
+                {
+                    AggregateType = agg.AggregateType.ToString(),
+                    ImplementationType = agg.ImplementationType.ToString(),
+                }).ToArray(),
+                TeclynVersion = version,
+            };
+
+            return Json(model, JsonRequestBehavior.AllowGet);
         }
     }
 }
